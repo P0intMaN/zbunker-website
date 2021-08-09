9cd024f7662350de08ce4d132eaee612c4c3a090
Pratheek U <pratheekastro3@gmail.com>
2021-08-09 20:48:06 +0530

    refactor: models.py

--- zbunker/models.py
+++ zbunker/models.py
@@ -1,9 +1,11 @@
 from zbunker import db, login_manager
 from flask_login import UserMixin
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
+
 
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,15 +14,13 @@ class User(db.Model, UserMixin):
     password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
-        return(f"{self.username}, {self.email}")
+        return f"{self.username}, {self.email}"
 
 
 class Courses(db.Model, UserMixin):
-    id = db.Column(db.Integer,primary_key=True)
-    course_name = db.Column(db.Integer,primary_key=True, unique=True)
-    is_prime = db.Column(db.Boolean,default=True, unique=False)
-
-    
+    id = db.Column(db.Integer, primary_key=True)
+    course_name = db.Column(db.Integer, primary_key=True, unique=True)
+    is_prime = db.Column(db.Boolean, default=True, unique=False)
 
 
 db.create_all()
