0452dee3e7579d5a6171cba6324046e6f4f1f80d
Pratheek U <pratheekastro3@gmail.com>
2021-08-09 20:48:33 +0530

    refactor: forms.py

--- zbunker/forms.py
+++ zbunker/forms.py
@@ -6,32 +6,37 @@ from zbunker.models import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=3,max=30)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirmPassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Sign Up')
+    username = StringField(
+        "Username", validators=[DataRequired(), Length(min=3, max=30)]
+    )
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    confirmPassword = PasswordField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    submit = SubmitField("Sign Up")
 
-    def validate_username(self,username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
-            raise ValidationError('Hmm... Looks like the Username already exists. Please choose a different one')
-    
-    def validate_email(self,email):
+            raise ValidationError(
+                "Hmm... Looks like the Username already exists. Please choose a different one"
+            )
+
+    def validate_email(self, email):
         email = User.query.filter_by(email=email.data).first()
         if email:
-            raise ValidationError('Oops! The Email is taken. Please choose a different one')
-
-
+            raise ValidationError(
+                "Oops! The Email is taken. Please choose a different one"
+            )
 
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(),Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remMe = BooleanField('Remember Me')
-    login = SubmitField('Login')
+    email = StringField("Email", validators=[DataRequired(), Email()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    remMe = BooleanField("Remember Me")
+    login = SubmitField("Login")
 
-    
 
 class NewVideoForm(FlaskForm):
     pass
