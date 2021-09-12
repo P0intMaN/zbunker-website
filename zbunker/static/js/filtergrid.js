2623450cf5c1d9c0cf12b053362f58c5057a1165
Pratheek U <pratheekastro3@gmail.com>
2021-09-12 13:34:02 +0530

    Add filter functionality

--- zbunker/static/js/filtergrid.js
+++ zbunker/static/js/filtergrid.js
@@ -1,5 +1,21 @@
-var button = 1; // buttons value ranges from 1 to 5. Each number for a certain filter.
-var $container = $('gallery-content-center')
+$(document).ready(function(){
 
-// applying isotope script
-$container.isotope({itemSelector : 'img'});
+    // When the links are clicked
+    $('.filter-link').click(function(){
+        
+        // First, remove all the active filter tags from the link (if any)
+        $('.filter-link').removeClass('filter-active');
+        // Then, add the active filter tag for the current link
+        $(this).addClass('filter-active');
+
+        const value = $(this).attr('data-filter');
+        if (value == "filter-all"){
+            $('.imageBox').show('1000');
+        }
+        else{
+            $('.imageBox').not('.'+value).hide('1000')
+            $('.imageBox').filter('.'+value).show('1000')
+        }
+
+    })
+})
