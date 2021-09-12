88c738dda8adbc434158d80a5c4c6df87f28bc0d
Pratheek U <pratheekastro3@gmail.com>
2021-09-12 13:17:25 +0530

    Add js file for filtergrid

---
+++ zbunker/static/js/filtergrid.js
@@ -0,0 +1,5 @@
+var button = 1; // buttons value ranges from 1 to 5. Each number for a certain filter.
+var $container = $('gallery-content-center')
+
+// applying isotope script
+$container.isotope({itemSelector : 'img'});
