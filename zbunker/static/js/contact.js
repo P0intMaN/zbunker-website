9f2f2ae0545398e819012cd18f54e013aebb465b
Pratheek U <pratheekastro3@gmail.com>
2021-09-21 23:04:01 +0530

    Refactor js file

--- zbunker/static/js/contact.js
+++ zbunker/static/js/contact.js
@@ -4,12 +4,10 @@ let options = {
     threshold: 0.9,
   };
 
-
 // observer1
 let observer = new IntersectionObserver(callbackfun, options);
 let observed = document.querySelector('.trigger2')
 let element = document.querySelector('.shift2')
-console.log(element)
 function callbackfun(entries){
     entries.forEach(entries =>{
         if(entries.isIntersecting){
@@ -27,7 +25,6 @@ function callbackfun(entries){
 let observer2 = new IntersectionObserver(func, options);
 let observed2 = document.querySelector('.trigger3');
 let element2 = document.querySelector('.content2');
-console.log(element2)
 
 function func(entries){
     entries.forEach(entries =>{
@@ -39,9 +36,11 @@ function func(entries){
                 element2.classList.add('appear')
             }
         }
-    })
+    })  
 }
+
 
 // calling observers
 observer.observe(observed);
 observer2.observe(observed2);
+
