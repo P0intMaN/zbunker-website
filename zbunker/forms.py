9070a4add0b45b3d4606faf1d4d4d9e18e459f5d
Pratheek U <pratheekastro3@gmail.com>
2021-10-20 11:34:16 +0530

    Add(forms.py): Verify and Reset Password forms

--- zbunker/forms.py
+++ zbunker/forms.py
@@ -40,5 +40,30 @@ class OTPForm(FlaskForm):
     sendOTP = SubmitField("SEND OTP")
 
 
+class VerifyOTPForm(FlaskForm):
+    OTP = StringField("OTP")
+    verifyOTP = SubmitField("Verify")
+
+    def validate_OTP(self, OTP):
+        user = (
+            OTPModel.query.filter_by(email=session["user_email"])
+            .order_by(OTPModel.id.desc())
+            .first()
+        )
+        if user:
+            if user.otp != OTP.data:
+                raise ValidationError("Incorrect OTP. Try Again")
+        else:
+            raise ValidationError("Incorrect OTP. Try Again!")
+
+
+class ResetPasswordForm(FlaskForm):
+    password = StringField("New Password", validators=[DataRequired()])
+    confirmpass = StringField(
+        "Confirm Password", validators=[DataRequired(), EqualTo("password")]
+    )
+    confirm = SubmitField("Confirm")
+
+
 class NewVideoForm(FlaskForm):
     pass
